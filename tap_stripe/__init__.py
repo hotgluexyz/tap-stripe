951b05a7bf899a0ea817667c4d41e6a700651152
Hassan Syyid <hassansyyid@gmail.com>
2022-01-30 22:00:20 -0500

    Start adding promotion_codes

--- tap_stripe/__init__.py
+++ tap_stripe/__init__.py
@@ -121,6 +121,8 @@ class Context():
     @classmethod
     def is_selected(cls, stream_name):
         stream = cls.get_catalog_entry(stream_name)
+        if stream is None:
+            return False
         stream_metadata = metadata.to_map(stream['metadata'])
         return metadata.get(stream_metadata, (), 'selected')
 
